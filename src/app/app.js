db7e8bd5834de5975183009a85f187b18bfc3756
Little-Tower <higuazu@gmail.com>
2020-06-25 23:24:04 +0200

    First fronted

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,68 @@
 import React, {Component} from 'react';
 
 class App extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            title: '',
+            description: ''
+        };
+        this.addTask = this.addTask.bind(this);
+    }
+
+    addTask(e){
+        console.log(this.state);
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+
     render(){
         return(
-            <h1>Hello world</h1>
+            <div>
+                {/* NAVEGATION*/}
+               <nav className="light-blue darken-4">
+                    <div className="container">
+                        <a className="brand-logo" href="/">Mern Stack</a>
+                    </div>
+               </nav>
+
+                {/*MAIN APP*/}
+               <div className="container">
+                    <div className="row">
+                        <div className="col s5">
+                            <div className="card">
+                                <div className="card-content">
+                                    <form onSubmit="{this.addTask}">
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                                <input type="text" placeholder="Task Title"/>
+                                            </div>
+                                        </div>
+
+                                        <div className="row">
+                                            <div className="input-field col s12">
+                                               <textarea placeholder="Description" className="materialize-textarea"></textarea>
+                                            </div>
+                                        </div>
+
+                                        <div className="row">
+                                            <div className="col s12">
+                                                <button type="submit" className="btn waves-effect waves-light light-blue darken-4">
+                                                    Send<i class="material-icons right">send</i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                       
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col s5"></div>
+                    </div>
+               </div>
+            </div>
         )
     }
 }
